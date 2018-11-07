4fbc6482e03d0d057e4769e3182239f516888b9e
Nazar Kuradovets <Nazar@MacBook-Air-Liliana.local>
2018-11-07 20:57:59 +0200

    First commit

---
+++ src/main/java/TestBase.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+
+    private void setUp() {
+
+
+        WebDriver driver = new ChromeDriver();
+    }
+
+
+    public static void main(String[] args) {
+        System.setProperties("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+
+    }
+}
